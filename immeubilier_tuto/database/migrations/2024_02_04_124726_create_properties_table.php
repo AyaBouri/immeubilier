e6d07ba677481993dd3cda35f9fb9f35e8b372ad
Aya Bouri <aya.bouri1@gmail.com>
2024-02-04 13:48:42 +0100

    projet

--- immeubilier_tuto/database/migrations/2024_02_04_124726_create_properties_table.php
+++ immeubilier_tuto/database/migrations/2024_02_04_124726_create_properties_table.php
@@ -13,14 +13,6 @@ return new class extends Migration
     {
         Schema::create('properties', function (Blueprint $table) {
             $table->id();
-            $table->string('title');
-            $table->longText('description');
-            $table->integer('surface');
-            $table->integer('rooms');
-            $table->integer('bedrooms');
-            $table->integer('floor');
-            $table->integer('price');
-            $table->string('city');
             $table->timestamps();
         });
     }
