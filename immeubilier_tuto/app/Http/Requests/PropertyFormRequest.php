c8dfef8c93532352d09e47733e54ed0b27df0824
Aya Bouri <aya.bouri1@gmail.com>
2024-02-04 22:18:47 +0100

    update

--- immeubilier_tuto/app/Http/Requests/PropertyFormRequest.php
+++ immeubilier_tuto/app/Http/Requests/PropertyFormRequest.php
@@ -11,7 +11,7 @@ class PropertyFormRequest extends FormRequest
      */
     public function authorize(): bool
     {
-        return false;
+        return true;
     }
 
     /**
@@ -22,7 +22,17 @@ class PropertyFormRequest extends FormRequest
     public function rules(): array
     {
         return [
-            //
+            'title'=>['require','min:8'],
+            'description'=>['require','min:8'],
+            'surface'=>['require','integer','min:10'],
+            'rooms'=>['require','integer','min:1'],
+            'bedrooms'=>['require','integer','min:0'],
+            'floor'=>['require','integer','min:0'],
+            'price'=>['require','integer','min:0'],
+            'city'=>['require','min:8'],
+            'address'=>['require','min:8'],
+            'postal_code'=>['require','min:8'],
+            'sold'=>['require','boolean']
         ];
     }
 }
