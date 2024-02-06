e2115d4bd6d4828a3dc373ca5807b79c6621e33e
Aya Bouri <aya.bouri1@gmail.com>
2024-02-06 14:50:05 +0100

    update

--- immeubilier_tuto/app/Http/Controllers/PropertyController.php
+++ immeubilier_tuto/app/Http/Controllers/PropertyController.php
@@ -7,13 +7,19 @@ class PropertyController extends Controller
 {
     public function index(SearchPropertiesRequest $request){
         $query=Property::query();
-        if($request->has('price')) {
-            $query = Property::query();
-            if ($request->has('price')) {
-                $query = $query->where('price', '<=', $request->input('price'));
-            }
-            $properties = $query->paginate(16);
-            return view('property.index', compact('properties'));
+        if($price=$request->validated('price')) {
+            $query = $query->where('price', '<=', $price);
+            /*$properties = $query->paginate(16);
+            return view('property.index', compact('properties'));*/
+        }
+        if ($surface=$request->validated('surface')) {
+            $query = $query->where('surface', '>=', $surface);
+        }
+        if ($rooms=$request->validated('rooms')) {
+            $query = $query->where('rooms', '>=',$rooms);
+        }
+        if ($title=$request->validated('title')) {
+            $query = $query->where('title', 'like', "%{$title}%");
         }
         //$properties=Property::paginate(16);
         return view('property.index',[
